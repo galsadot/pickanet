e9c8477b33702079c68d6b8f436fc6b755666d80
gal <gal@compie.co.il>
2017-04-11 18:24:09 +0300

    pickanet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,7 +25,6 @@ import { FormComponent } from './form/form.component';
     StandartBtnComponent,
     AppCartComponent,
     CartPageComponent,
-    CoimngComponent,
     PaymentComponent,
     FormComponent
   ],
